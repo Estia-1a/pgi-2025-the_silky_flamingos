89c029f8b873b3f524caa1be265de773e231daf4
Mattéo ROBIN <matteo.robin@estia.local>
2025-05-16 16:04:13 +0200

    dimension

--- src/features.c
+++ src/features.c
@@ -14,3 +14,10 @@
 void helloWorld() {
     printf("Hello World bro!");
 }
+
+void dimension(char *source_path){
+    
+    int width=0, height=0;
+    read_image_data(const char source_path, &data, &width, &height, &channel_count);
+    printf("dimension: %d, %d",width,height);
+}
--- src/features.h
+++ src/features.h
@@ -4,5 +4,6 @@
 
 void helloWorld();
 
+void dimension (char *source_path);
 
 #endif
