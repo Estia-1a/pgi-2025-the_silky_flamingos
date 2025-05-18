37b7aece2e214d583b2f80b919d06a6738365bb5
valpr1 <portalvalentin1@gmail.com>
2025-05-18 18:45:41 +0200

    #24 : Convertion logic in getPixel -> Indata: 2D pixel to 1D channel

--- src/utils.c
+++ src/utils.c
@@ -10,10 +10,20 @@
 pixelRGB *getPixel( unsigned char* data, const unsigned int width, const unsigned int height, const unsigned int n, const unsigned int x, const unsigned int y ) {
     pixelRGB *pixel;
     
-    printf("Pixel choisi (test): %d, %d, %d\n", x, y, n);
-    pixel->R = data[x, y, 0];
-    pixel->G = data[x, y, 1];
-    pixel->B = data[x, y, 2];
+    // Convert the data 2D coordinates to 1D and thus, to "channel" coordinates
+    int index = (y * width + x) * n;
+
+    printf("Index R: %d\n", index);
+    printf("Value R: %d\n", data[index]);
+    pixel->R = data[index];
+
+    printf("Index G: %d\n", index + 1);
+    printf("Value G: %d\n", data[index + 1]);
+    pixel->G = data[index + 1];
+
+    printf("Index B: %d\n", index + 2);
+    printf("Value B: %d\n", data[index + 2]);
+    pixel->B = data[index + 2];
 
     return pixel;
 }
