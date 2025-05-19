da7d8b79c408157794969003265bdbf63024d594
valpr1 <portalvalentin1@gmail.com>
2025-05-19 14:04:49 +0200

    #24 : malloc function introduced for pixel variable

--- src/utils.c
+++ src/utils.c
@@ -7,22 +7,17 @@
  * 
  */
 
-pixelRGB *getPixel( unsigned char* data, const unsigned int width, const unsigned int height, const unsigned int n, const unsigned int x, const unsigned int y ) {
+pixelRGB *getPixel( unsigned char* data, const unsigned int width, const unsigned int height, const unsigned int n, const unsigned int x, const unsigned int y) {
     pixelRGB *pixel;
     
     // Convert the data 2D coordinates to 1D and thus, to "channel" coordinates
     int index = (y * width + x) * n;
 
-    printf("Index R: %d\n", index);
-    printf("Value R: %d\n", data[index]);
+    // Allocate memory for the pixel (using malloc)
+    pixel = malloc(3 * sizeof(unsigned char));
+
     pixel->R = data[index];
-
-    printf("Index G: %d\n", index + 1);
-    printf("Value G: %d\n", data[index + 1]);
     pixel->G = data[index + 1];
-
-    printf("Index B: %d\n", index + 2);
-    printf("Value B: %d\n", data[index + 2]);
     pixel->B = data[index + 2];
 
     return pixel;
