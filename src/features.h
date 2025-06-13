8c4a526f093bd2819403e938a33c2d18bdb8e4d6
ElouSoub <elouan.soubrane@etu.estia.fr>
2025-06-13 15:24:53 +0200

    add void second_line

--- src/features.h
+++ src/features.h
@@ -10,7 +10,7 @@ void first_pixel (char *source_path);
 
 void tenth_pixel (char *source_path);
 
-second_line(char *source_path);
+void second_line(char *source_path);
 
 void rotate_cw(char *source_path);
 
