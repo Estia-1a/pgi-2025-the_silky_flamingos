9f8bad4e22525330d5ab250c5ba6c81f1169cbf3
ElouSoub <elouan.soubrane@etu.estia.fr>
2025-06-13 15:13:41 +0200

    implement rotate_cw into features.c

--- src/features.c
+++ src/features.c
@@ -35,3 +35,27 @@ void tenth_pixel (char *source_path){
     read_image_data(source_path, &data, &width, &height, &channel_count);
     printf("RGB : %d, %d, %d", data[27], data[28], data[29]);
 }
+
+void rotate_cw(char *source_path) {
+    unsigned char* data = NULL;
+    int width = 0, height = 0, channel_count = 0;
+    read_image_data(source_path, &data, &width, &height, &channel_count);
+    unsigned char* rotated_data = malloc(width * height * channel_count);
+    if (!rotated_data) {
+        printf("Erreur d'allocation mémoire\n");
+        return;
+    }
+    for (int y = 0; y < height; ++y) {
+        for (int x = 0; x < width; ++x) {
+            for (int c = 0; c < channel_count; ++c) {
+                int src_index = (y * width + x) * channel_count + c;
+                int dest_index = ((x * height) + (height - 1 - y)) * channel_count + c;
+                rotated_data[dest_index] = data[src_index];
+            }
+        }
+    }
+    write_bmp_image("image_out.bmp", rotated_data, height, width, channel_count);
+    free(data);
+    free(rotated_data);
+}
+
