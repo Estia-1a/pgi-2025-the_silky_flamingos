8ed31aa646a01fad3356db3cba4911ec56c2cdcd
Mattéo ROBIN <matteo.robin@estia.local>
2025-06-19 22:23:48 +0200

    changement get pixels

--- src/utils.h
+++ src/utils.h
@@ -14,7 +14,23 @@ typedef struct _pixelRGB {
   unsigned char B;
 } pixelRGB ;
 
-pixelRGB * get_pixel( unsigned char* data, const unsigned int width, const unsigned int height, const unsigned int n, const unsigned int x, const unsigned int y );
 
+pixelRGB *getPixel( unsigned char* data, const unsigned int width, const unsigned int height, const unsigned int n, const unsigned int x, const unsigned int y) {
+
+    // Check if the pixel coordonates are in the image
+    if (x >= width || y >= height) {
+        return NULL; 
+    }
+    
+    // Check if there is no data
+    else if (data == NULL) {
+        return NULL;
+    }
+
+    else {
+        return (pixelRGB *) (data + (y * width + x) * n);
+    }
+
+}
 
 #endif
