32d17083b730a2e7188bbee4315114a1437eeff0
Mattéo ROBIN <matteo.robin@estia.local>
2025-06-19 14:32:15 +0200

    tuto_4.2

--- src/utils.c
+++ src/utils.c
@@ -7,3 +7,22 @@
  * 
  */
 
+pixelRGB* get_pixel(const unsigned char* data, int width, int height, int n, int x, int y) {
+    if (data==NULL|| x<0 || y<0 || x>=width || y>= height || n<3){
+        return NULL;
+    }
+    
+    int index = (y * width + x) * channels;
+
+    pixelRGB* pixel = malloc(sizeof(pixelRGB));
+    if (pixel==NULL){
+        return NULL;
+    }
+
+    pixel->R = data[index];
+    pixel->G = data[index + 1];
+    pixel->B = data[index + 2];
+
+    return pixel;
+
+}
